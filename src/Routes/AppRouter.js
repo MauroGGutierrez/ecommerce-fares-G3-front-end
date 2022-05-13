206997e237a47181dc34f9d2bad58592586be5df
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-05-13 19:19:05 -0300

    chore: navbar almost finished

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductCards from "../pages/ProductCards";
 import CreateProduct from "../pages/CreateProduct";
+import NavBar from "../pages/NavBar";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductCards />} />
-        <Route path="/createProduct" element={<CreateProduct />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/Productos" element={<ProductCards />} />
+          <Route path="/createProduct" element={<CreateProduct />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
---
+++ src/components/_styles.js
@@ -0,0 +1,20 @@
+export const navbarstyles = {
+  navbarLink: {
+    display: "flex",
+    alignItems: "center",
+    fontWeight: "500",
+    fontSize: "17px",
+  },
+  hover: {
+    fontWeight: "bold",
+    fontSize: "20px",
+    color: "#d00000",
+  },
+  buttons: {
+    padding: "7px",
+    borderRadius: "6px",
+  },
+  active: {
+    transform: "scale(0.96)",
+  },
+};
