d40cc6c3292ebc7ff1f70c372e88c9b912b956ec
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-05-18 17:22:29 -0300

    chore:add categorys

---
+++ src/data/FilterCategory.js
@@ -0,0 +1,68 @@
+import { Box, List, ListItem, Text } from "@chakra-ui/react";
+import React from "react";
+
+const FilterCategory = () => {
+  return (
+    <Box ml={2} mt={8}>
+      <Text fontSize={"2xl"} fontWeight={"semibold"} as="i">
+        Categorias
+      </Text>
+      <List spacing={2} mt={"0.7rem"} mb={"0.9rem"}>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Placas
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Procesadores
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Memoria RAM
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Coolers y Ventiladores
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Gabinetes
+        </ListItem>
+      </List>
+      <Text fontSize={"2xl"} fontWeight={"semibold"} as="i">
+        Condicion
+      </Text>
+      <List spacing={2} mt={"0.7rem"} mb={"0.9rem"}>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Nuevo
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Usado
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Reparado
+        </ListItem>
+      </List>
+      <Text fontSize={"2xl"} fontWeight={"semibold"} as="i">
+        Marcas
+      </Text>
+      <List spacing={2} mt={"0.7rem"}>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          AMD
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Intel
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Redragon
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Rezer
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Gigabyte
+        </ListItem>
+        <ListItem cursor="pointer" _hover={{ color: "red" }}>
+          Corsair
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+
+export default FilterCategory;
--- src/pages/ProductCards.js
+++ src/pages/ProductCards.js
@@ -1,26 +1,30 @@
-import { Container, Grid } from "@chakra-ui/react";
+import { Container, Flex, Grid } from "@chakra-ui/react";
 import React from "react";
 import ContentProduct from "../components/contentProduct";
 import { contentCards } from "../data/contentCards";
+import FilterCategory from "../data/FilterCategory";
 
-const ProductCards = ({ DataSearch }) => {
+const ProductCards = () => {
   return (
-    <Container p="0" m="0 auto" maxW={"90%"}>
-      <Grid
-        gridTemplateColumns="repeat(auto-fill, minmax(250px,1fr))"
-        gap="2.9rem"
-      >
-        {contentCards.map((cards) => (
-          <ContentProduct
-            key={cards.id}
-            title={cards.title}
-            description={cards.description}
-            price={cards.price}
-            imgCards={cards.IMG}
-          />
-        ))}
-      </Grid>
-    </Container>
+    <Flex>
+      <FilterCategory />
+      <Container p="0" m="0 auto" maxW={"90%"} mr={2}>
+        <Grid
+          gridTemplateColumns="repeat(auto-fill, minmax(250px,1fr))"
+          gap="2rem"
+        >
+          {contentCards.map((cards) => (
+            <ContentProduct
+              key={cards.id}
+              title={cards.title}
+              description={cards.description}
+              price={cards.price}
+              imgCards={cards.IMG}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </Flex>
   );
 };
 
